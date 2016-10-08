871dbacd56722bec695e41fddd013716443b986f
Samuel Ramos Barroso <alu0100815146@ull.edu.es>
2016-10-08 16:25:17 +0000

    Ejemplo completo

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var session      = require('express-session');
 // configuration ===============================================================
 mongoose.connect('mongodb://localhost/data'); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
